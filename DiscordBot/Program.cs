3507ce0acd56129bf99c14d312b257d9f5c49b4f
Joshua Jones <kilauea.jones@gmail.com>
2018-04-08 16:43:35 -0500

    Added plumbing for commands

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DiscordBot
 {
@@ -21,11 +24,25 @@ namespace DiscordBot
                 Console.WriteLine(message);
                 return Task.CompletedTask;
             };
+
+            var serviceProvider = new ServiceCollection()
+                .BuildServiceProvider();
+            var commandService = new CommandService();
+            await commandService.AddModulesAsync(Assembly.GetEntryAssembly());
             
-            client.MessageReceived += async message =>
+            client.MessageReceived += async rawMessage =>
             {
-                if (message.Content == "!ping")
-                    await message.Channel.SendMessageAsync("Pong!");
+                if (!(rawMessage is SocketUserMessage message)) 
+                    return;
+                
+                var argPos = 0;
+                if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))) 
+                    return;
+                
+                var context = new CommandContext(client, message);
+                var result = await commandService.ExecuteAsync(context, argPos, serviceProvider);
+                if (!result.IsSuccess)
+                    Console.WriteLine("Failed to run command: {0}", result.ErrorReason);
             };
 
             var token = File.ReadAllText("token.txt");
