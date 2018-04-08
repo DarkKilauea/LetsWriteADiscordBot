3a5d3f2800403197438e88b9877bd723fd1e4b88
Joshua Jones <kilauea.jones@gmail.com>
2018-04-08 16:24:39 -0500

    Initial project setup

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace DiscordBot
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            var client = new DiscordSocketClient();
+            client.Log += message =>
+            {
+                Console.WriteLine(message);
+                return Task.CompletedTask;
+            };
+
+            var token = File.ReadAllText("token.txt");
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+    }
+}
