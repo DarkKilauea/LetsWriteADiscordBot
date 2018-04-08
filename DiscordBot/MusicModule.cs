f303f4b1032dc65657514bc21971596191ef2f16
Joshua Jones <kilauea.jones@gmail.com>
2018-04-08 18:33:35 -0500

    Added command to play music

---
+++ DiscordBot/MusicModule.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Audio;
+using Discord.Commands;
+
+namespace DiscordBot
+{
+    public class MusicModule : ModuleBase
+    {
+        [Command("play", RunMode = RunMode.Async)]
+        public async Task PlayInChannel(IVoiceChannel channel = null)
+        {
+            // Get the audio channel
+            channel = channel ?? (Context.Message.Author as IGuildUser)?.VoiceChannel;
+            if (channel == null)
+            {
+                await ReplyAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); 
+                return;
+            }
+
+            // For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+            using (var audioClient = await channel.ConnectAsync())
+            {
+                await SendAsync(audioClient, "Pump.mp3");
+            }
+        }
+        
+        private static Process CreateStream(string path)
+        {
+            var ffmpeg = new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $"-i {path} -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            };
+            
+            return Process.Start(ffmpeg);
+        }
+        
+        private static async Task SendAsync(IAudioClient client, string path)
+        {
+            // Create FFmpeg using the previous example
+            var ffmpeg = CreateStream(path);
+            var output = ffmpeg.StandardOutput.BaseStream;
+            var discord = client.CreatePCMStream(AudioApplication.Music);
+            await output.CopyToAsync(discord);
+            await discord.FlushAsync();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # LetsWriteADiscordBot
+
 Content for a talk introducing writing bots for the Discord platform
+
+## Music Playback
+
+This step requires that libopus, libsodium, and ffmpeg are installed
+
+On macOS, you can use homebrew to install these via:
+
+``` bash
+brew install opus
+brew install libsodium
+brew install ffmpeg
+```
+
+On windows, you will need to build or download the dlls and drop them in the
+`DiscordBot\bin\Debug\netcoreapp2.0\` directory.
+
+For linux, use your package manager to install these dependencies.
+
+## Credits
+
+Pump Kevin MacLeod (incompetech.com)
+Licensed under Creative Commons: By Attribution 3.0 License
+http://creativecommons.org/licenses/by/3.0/
