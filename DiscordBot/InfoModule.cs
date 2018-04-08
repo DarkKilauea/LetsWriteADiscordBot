6a8dd1a18529154b69abeed1711f970bf16f03f5
Joshua Jones <kilauea.jones@gmail.com>
2018-04-08 16:56:26 -0500

    Added back in the ping command as part of a command module

---
+++ DiscordBot/InfoModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordBot
+{
+    public class InfoModule : ModuleBase
+    {
+        [Command("ping"), Summary("Returns a pong")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong!");
+        }
+    }
+}
